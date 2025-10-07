a6a88650d2ff8cd6a2cba3b1775d0b13d8eb9465
sboyarkin <sergey.boyarkin@mail.ru>
2025-10-07 17:58:29 +0500

    create user directory

--- backend/cloud/models.py
+++ backend/cloud/models.py
@@ -2,12 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+
+    if hasattr(instance, 'user'):
+        user_id = instance.user.id
+
+    elif isinstance(instance, User):
+        user_id = instance.id
+    else:
+        user_id = 'unknown'
+
+    return f'uploads/{user_id}/{filename}'
+
 class File(models.Model):
     name = models.CharField(max_length=255)
-    file_name = models.CharField(max_length=255)
-    url = models.URLField(max_length=255)
-    file = models.FileField(upload_to='files')
-    coment = models.TextField()
+    unique_name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255, blank=True,)
+    file = models.FileField(upload_to=user_directory_path)
+    comment = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
--- backend/cloud/serializers.py
+++ backend/cloud/serializers.py
@@ -4,3 +4,11 @@ class FileViewSetSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = '__all__'
+        read_only_fields = ('name', 'unique_name', 'url',)
+
+    def create(self, validated_data):
+        name = validated_data.get('file')
+        validated_data['name'] = name
+        print(name)
+        print(validated_data)
+        return super().create(validated_data)
