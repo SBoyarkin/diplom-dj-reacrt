7b519f76a0183396bcfc0e5115d576b0a8b61ad3
sboyarkin <sergey.boyarkin@mail.ru>
2025-10-08 11:51:53 +0500

    create token auth

---
+++ README.md
@@ -0,0 +1,16 @@
+### Djoser auth endpoint
+
+/users/
+/users/me/
+/users/resend_activation/
+/users/set_password/
+/users/reset_password/
+/users/reset_password_confirm/
+/users/set_username/
+/users/reset_username/
+/users/reset_username_confirm/
+/token/login/ (Token Based Authentication)
+/token/logout/ (Token Based Authentication)
+/jwt/create/ (JSON Web Token Authentication)
+/jwt/refresh/ (JSON Web Token Authentication)
+/jwt/verify/ (JSON Web Token Authentication)
--- backend/cloud/views.py
+++ backend/cloud/views.py
@@ -3,10 +3,23 @@ from rest_framework import viewsets
 
 from cloud.serializers import FileViewSetSerializer
 from cloud.models import File
+from rest_framework.permissions import IsAuthenticated
 
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileViewSetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        print(dir(self.request.user))
+        if self.request.user.is_authenticated:
+            if self.request.user.is_superuser:
+                return File.objects.all()
+            else:
+                return File.objects.filter(user=self.request.user)
+
+
+
 
 # Create your views here.
