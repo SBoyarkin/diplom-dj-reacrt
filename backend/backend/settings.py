ea6dadd11841f4021a8ddf7ceb9b31af48c96a83
sboyarkin <sergey.boyarkin@mail.ru>
2025-10-08 17:53:45 +0500

    get token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
@@ -44,8 +45,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +63,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "http://localhost:5173",
+]
 
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,28 @@
 import S from './Login.module.css'
+import {useEffect, useState} from "react";
 export const Login = () => {
+
+    const [login, updateLoginHandler] = useState('')
+
+    const submitLogin = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const entries = formData.entries()
+        const obj = Object.fromEntries(entries)
+        updateLoginHandler(obj)
+    }
+
+    useEffect(() => {
+        if (login.username && login.password) {
+        fetch('http://127.0.0.1:8000/auth/token/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json", },
+            body: JSON.stringify(login)
+        }).then((response) => {
+              return response.json()})
+    }}, [login])
+
     return(
         <>
         <div className={S.main}>
@@ -7,10 +30,10 @@ export const Login = () => {
                 left
             </div>
             <div className={S.right}>
-                <form className={S.form}>
+                <form name='login' className={S.form} onSubmit={submitLogin}>
                     <div className={S.title}>Добро пожаловать</div>
-                    <input className={S.input} type='text'/>
-                    <input className={S.input} type='text'/>
+                    <input name='username' className={S.input} type='text'/>
+                    <input name='password' className={S.input} type='text'/>
                     <button className={S.btn} type="submit">Войти</button>
                     <a>Регистрация</a>
                 </form>
