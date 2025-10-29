f1b6a24516fe6b8b7737a44576b4709bdc97163e
sboyarkin <sergey.boyarkin@mail.ru>
2025-10-29 16:32:55 +0500

    Final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,7 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/cloud_storage/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/cloud_storage/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
