99a3fe8a353509789fd9508ebac560c84788c34c
sboyarkin <sergey.boyarkin@mail.ru>
2025-10-30 14:50:02 +0500

    change staff

--- frontend/src/components/userCard/UserCard.jsx
+++ frontend/src/components/userCard/UserCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import S from './UserCard.module.css';
 import {apiClient} from "../../customRequest.js";
-
+import {useSelector} from "react-redux";
 
 
 export const UserCard = ({ user, isSelected, onSelect, onDelete }) => {
+    const me = useSelector(store => store.user)
     const handleDelete = (e) => {
         e.stopPropagation();
 
@@ -22,8 +23,27 @@ export const UserCard = ({ user, isSelected, onSelect, onDelete }) => {
         }
     };
 
+    
+    const handleChangeRole = (e) => {
+        e.stopPropagation()
+         apiClient.patch(`change-status/${user.id}/`, {is_staff: !user.is_staff})
+
+                .then(response => {
+                    if (response.status === 200) {
+                        onDelete();
+                    }
+                })
+                .catch(error => {
+                    console.error('Error deleting user:', error);
+                    alert(`Не удалось сменить роль у пользователя ${user.username}`);
+                });
+
+      
+    }
+    
     return (
         <div
+
             className={`${S.userCard} ${isSelected ? S.selected : ''}`}
             onClick={() => onSelect(user)}
         >
@@ -34,13 +54,26 @@ export const UserCard = ({ user, isSelected, onSelect, onDelete }) => {
                     {user.is_staff ? 'Администратор' : 'Пользователь'}
                 </span>
             </div>
-            <button
-                className={S.deleteButton}
-                onClick={handleDelete}
-                title="Удалить пользователя"
-            >
-                ×
-            </button>
+            {me.id !== user.id ?
+                <>
+                    <button
+                        className={S.statusButton}
+                        onClick={handleChangeRole}
+                        title={user.is_staff ? 'Снять права администратора' : 'Назначить администратором'}
+                    >
+                        {user.is_staff ? '⬇️ Понизить до пользователя' : '⬆️ Повысить до администратора'}
+                    </button>
+                    <button
+                        className={S.deleteButton}
+                        onClick={handleDelete}
+                        title="Удалить пользователя"
+                    >
+                        ×
+                    </button>
+
+
+                </>
+                : null}
         </div>
     );
 };
